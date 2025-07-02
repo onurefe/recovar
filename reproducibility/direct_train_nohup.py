f41c285e532a77b57d746d065db40480eb55d4db
Ege Adıgüzel <egeisxol@gmail.com>
2025-07-02 16:30:18 +0300

    Created: direct_train_nohup script for the new training loop

---
+++ reproducibility/direct_train_nohup.py
@@ -0,0 +1,25 @@
+from direct_trainer import DirectTrainer
+from recovar import RepresentationLearningSingleAutoencoder, RepresentationLearningDenoisingSingleAutoencoder, RepresentationLearningMultipleAutoencoder
+
+from directory import (
+    STEAD_WAVEFORMS_HDF5_PATH,
+    STEAD_METADATA_CSV_PATH,
+    INSTANCE_EQ_WAVEFORMS_HDF5_PATH,
+    INSTANCE_NOISE_WAVEFORMS_HDF5_PATH,
+    INSTANCE_EQ_METADATA_CSV_PATH,
+    INSTANCE_NOISE_METADATA_CSV_PATH,
+    PREPROCESSED_DATASET_DIRECTORY,
+)
+
+trainer = DirectTrainer(dataset='stead',
+    dataset_time_window=60.0,  # STEAD has 60s windows
+    model_time_window=30.0
+)
+
+trainer.create_subsampled_datasets(
+    dataset='stead',
+    output_dir=PREPROCESSED_DATASET_DIRECTORY,
+    noise_percentages=[20, 30, 40, 50, 60, 70, 80, 90, 100],
+    subsampling_factor=1.0, 
+    maintain_constant_size=True
+)
